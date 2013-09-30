3cb1eb9ea969b8f7dc883801753039ddeeb38efe
kristenmills <kristen@kristen-mills.com>
2013-09-30 10:08:52 -0400

    Got the before portion working

--- lib/night_vision.rb
+++ lib/night_vision.rb
@@ -1,5 +1,19 @@
 require "night_vision/version"
 
 module NightVision
-  # Your code goes here...
+  DIVIDER = "------------------------------------------------"
+  def self.before(*classes)
+    classes.each do |klass|
+      klass.instance_methods.each do |method|
+        m = klass.instance_method(method)
+        klass.send(:define_method, method) do |*args, &block|
+          puts DIVIDER
+          puts "Invoking #{method}"
+          puts "Actual arguments are: #{args}"
+          puts DIVIDER
+          m.bind(self).(*args, &block)
+        end
+      end
+    end
+  end
 end
